5aa3ba6ffa20f0546154b3cb62e831635322c982
reeWorlds <43860753+reeWorlds@users.noreply.github.com>
2020-01-17 09:42:22 +0200

    Add files via upload
    Add button class

---
+++ Button.cpp
@@ -0,0 +1,72 @@
+#include "Button.h"
+
+Button::Button(float x, float y, float width, float height, Font* font, string text,
+	Color idleColor, Color hoverColor, Color activeColor) :
+	idleColor(idleColor), hoverColor(hoverColor), activeColor(activeColor)
+{
+	buttonState = BUTTON_STATE::BUTTON_IDLE;
+
+	shape.setPosition(Vector2f(x, y));
+	shape.setSize(Vector2f(width, height));
+
+	this->font = font;
+	this->text.setFont(*this->font);
+	this->text.setString(text);
+	this->text.setFillColor(Color::White);
+	this->text.setCharacterSize(12);
+	this->text.setPosition(
+		shape.getPosition().x + shape.getGlobalBounds().width / 2.0 - this->text.getGlobalBounds().width / 2.0,
+		shape.getPosition().y + shape.getGlobalBounds().height / 2.0 - this->text.getGlobalBounds().height / 2.0
+		);
+}
+
+Button::~Button()
+{
+
+}
+
+bool Button::isPressed()
+{
+	if (buttonState == BUTTON_STATE::BUTTON_ACTIVE)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void Button::updateState(Vector2f mousePosition)
+{
+	buttonState = BUTTON_STATE::BUTTON_IDLE;
+
+	if (shape.getGlobalBounds().contains(mousePosition))
+	{
+		buttonState = BUTTON_STATE::BUTTON_HOVER;
+
+		if (Mouse::isButtonPressed(Mouse::Left))
+		{
+			buttonState = BUTTON_STATE::BUTTON_ACTIVE;
+		}
+	}
+
+	switch (buttonState)
+	{
+	case BUTTON_STATE::BUTTON_IDLE:
+		shape.setFillColor(idleColor);
+		break;
+	case BUTTON_STATE::BUTTON_HOVER:
+		shape.setFillColor(hoverColor);
+		break;
+	case BUTTON_STATE::BUTTON_ACTIVE:
+		shape.setFillColor(activeColor);
+		break;
+	default:
+		_ASSERT("ButtonError#1(wrong state)" == "");
+	}
+}
+
+void Button::draw(RenderWindow& window)
+{
+	window.draw(shape);
+	window.draw(text);
+}
---
+++ Button.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Constants.db"
+
+using namespace std;
+using namespace sf;
+
+enum BUTTON_STATE
+{
+	BUTTON_IDLE,
+	BUTTON_HOVER,
+	BUTTON_ACTIVE
+};
+
+
+class Button
+{
+	/*
+	This button is not very flexible and require attention
+	be accurate using it
+	*/
+
+private:
+
+	BUTTON_STATE buttonState;
+
+	RectangleShape shape;
+	Font* font;
+	Text text;
+
+	Color idleColor;
+	Color hoverColor;
+	Color activeColor;
+
+public:
+
+	Button(float x, float y, float width, float height, Font* font, string text,
+		Color idleColor, Color hoverColor, Color activeColor);
+	~Button();
+
+	bool isPressed();
+
+	void updateState(Vector2f mousePosition);
+	void draw(RenderWindow& window);
+
+};
